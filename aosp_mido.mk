8003242d7188b1bb2a50a3cf8f2c6ea05d6b0130
thp1997 <tirthpatel49@gmail.com>
2017-03-26 04:44:41 +0000

    AEX Bringup

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := aosp_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
