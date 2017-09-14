47755480fa8825de938e6d2f313de19b63f6e47f
thp1997 <tirthpatel49@gmail.com>
2017-09-14 15:26:16 +0000

    Revert "TEMP: Remove init.qcom.bt.sh to fix WiFI and BT init (Partitally)"
    This reverts commit 7588fdbbc8ad0a81eaa05f1361268b8d9f4b05de.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -27,9 +27,7 @@
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
-
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
 #endif
--- device.mk
+++ device.mk
@@ -253,6 +253,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
+    init.qcom.bt.sh \
     init.qcom.post_boot.sh
 
 
