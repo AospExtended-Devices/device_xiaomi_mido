4e2e4fa6df522ec5c37988624a1a9c4904bb0864
xyyx <xyyx@mail.ru>
2017-09-25 14:03:52 +0000

    mido: Add Missing mediaextractor.policy

--- device.mk
+++ device.mk
@@ -276,12 +276,12 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libcnefeatureconfig \
     libxml2 \
-    rild_socket \
-    Stk
+    rild_socket 
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
