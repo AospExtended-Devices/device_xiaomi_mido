5afc66a39e59186c78ff786e1bbdadd74a75a203
thp1997 <tirthpatel49@gmail.com>
2017-12-19 16:42:36 -0300

    mido: Update IMS props & Few Additional Flags
     * Taken from volte patch by @althafvly

--- device.mk
+++ device.mk
@@ -18,6 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 
+# HWUI
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
