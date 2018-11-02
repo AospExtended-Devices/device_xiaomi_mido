9ea4008783ca99d275f12848dfd4f3ba4ed2d0e0
Artem Borisov <dedsa2002@gmail.com>
2018-11-02 18:29:45 +0400

    mido: Define vendor security patch level
    * From xiaomi/tissot/tissot_sprout:8.1.0/OPM1.171019.026/V9.6.7.0.ODHMIFE:user/release-keys

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,9 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 872415232
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 
+# Security Patch Level
+VENDOR_SECURITY_PATCH := 2018-10-05
+
 # Sepolicy
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
